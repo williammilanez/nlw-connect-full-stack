0ffaa878943f9904c0027548e47aa4dfde16c568
William Milanez <william_milanez@hotmail.com>
2025-02-18 15:38:29 -0300

    aula02 - add arquivo style.css e estilização inicial do projeto

--- script.js
+++ script.js
@@ -1,10 +1,91 @@
 const app = document.getElementById("app")
 
+// array, vetor
+const users = [
+    {
+        email: "test@test.com",
+        phone: "99999999999",
+        ref: 100,
+        refBy: null
+    },
+    {
+        email: "tust@tust.com",
+        phone: "99999999999",
+        ref: 200,
+        refBy: 100
+    },
+    {
+        email: 'tost@tost.com',
+        phone: '99999999999',
+        ref: 300,
+        refBy: 200
+    }
+]
+
+const getUser = (userData) => {
+    return users.find((user) => {
+      return user.email == userData.email
+    })
+}
+  
+const getTotalSubscribers = (userData) => {
+    const subs = users.filter((user) => {
+      return user.refBy == userData.ref
+    })
+
+    return subs.length
+}
+
+const showInvite = (userData) => {
+    app.innerHTML = `
+    <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
+
+    <div id="stats">
+        <h4>
+           ${getTotalSubscribers(userData)}
+        </h4>
+
+        <p>
+            Inscrições feitas!
+        </p>
+    </div>
+    `
+}
+
+const saveUser = (userData) => {
+    const newUser = {
+        ...userData,
+        ref: Math.round(Math.random() * 4000),
+        refBy: 100
+    }
+
+    users.push(newUser)
+
+    console.log(users)
+
+    return newUser
+}
+
 const formAction = () => {
     const form = document.getElementById("form")
+
     form.onsubmit = (event) => {
         event.preventDefault()
-        
+
+        const formData = new FormData(form)
+
+        const userData = {
+            email: formData.get("email"),
+            phone: formData.get("phone"),
+        }
+
+        const user = getUser(userData)
+        if (user) {
+            showInvite(user)
+        } else {
+            const newUser = saveUser(userData)
+            showInvite(newUser)
+        }
     }
 }
 
@@ -19,9 +100,12 @@ const startApp = () => {
         </button>
     </form>
     `
+
     app.innerHTML = content;
 
     formAction()
 }
 
 startApp()
+
+document.getElementById("logo").onclick = () => startApp()
